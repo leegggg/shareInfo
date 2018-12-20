94a5451f471b5ca274d62bb4428b3eec8702d1b0
LIN Yizhou <linyzh@cn.ibm.com>
2018-12-20 22:49:12 +0800

    tick

--- share/app/ticksUpdate.py
+++ share/app/ticksUpdate.py
@@ -31,7 +31,7 @@ def main():
 
     # Update Ticks
     logging.info("Daily Update Ticks")
-    start = datetime(year=2018, month=3, day=15)
+    start = datetime(year=2018, month=4, day=11)
     #end = start + timedelta(days=15)
     end = datetime.now()
     try:
